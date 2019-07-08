fb2ff9cead49f534534f51195bba842f0bdefdf9
Whyatt Foster <whyattfoster@gmail.com>
2019-07-08 12:34:52 -0400

    deploy

--- models/connection.js
+++ models/connection.js
@@ -15,10 +15,20 @@ const connectionString = process.env.MONGODB_URI || "mongodb://localhost/meal-pl
  *
  * NOTE: newUrlParser diables a deprecation warning
  */
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
-  });
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/<db-name>');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 
 /* Export the mongoose object.
@@ -28,3 +38,4 @@ mongoose.connect(connectionString, { useNewUrlParser: true})
  *
  */
 module.exports = mongoose
+
